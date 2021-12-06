9e5cec869e883136057b8ac8a2a619ac920f0855
Daniel-R23 <danielima1023@gmail.com>
2021-12-06 17:12:34 -0300

    add get person by id method

--- src/main/java/com/dio/personAPI/controller/PersonController.java
+++ src/main/java/com/dio/personAPI/controller/PersonController.java
@@ -3,12 +3,14 @@ package com.dio.personAPI.controller;
 import com.dio.personAPI.dto.request.PersonDTO;
 import com.dio.personAPI.dto.response.MessageResponseDTO;
 import com.dio.personAPI.entity.Person;
+import com.dio.personAPI.exception.PersonNotFoundException;
 import com.dio.personAPI.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -25,5 +27,20 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) { //Informa que tá vindo uma requisição do tipo pessoa
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @DeleteMapping
+    public void deleteById(@PathVariable Long id){
+        personService.deleteById(id);
     }
 }
--- src/main/java/com/dio/personAPI/service/PersonService.java
+++ src/main/java/com/dio/personAPI/service/PersonService.java
@@ -3,10 +3,15 @@ package com.dio.personAPI.service;
 import com.dio.personAPI.dto.request.PersonDTO;
 import com.dio.personAPI.dto.response.MessageResponseDTO;
 import com.dio.personAPI.entity.Person;
+import com.dio.personAPI.exception.PersonNotFoundException;
 import com.dio.personAPI.mapper.PersonMapper;
 import com.dio.personAPI.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service // Indica para o Spring gerenciar uma classe com todas as regras de negócio da nossa aplicação
 public class PersonService {
@@ -28,5 +33,22 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id).orElseThrow(()-> new PersonNotFoundException(id));
+        return personMapper.toDTO(person);
+
+    }
+
+    public void deleteById(Long id) {
+        personRepository.deleteById(id);
     }
 }
